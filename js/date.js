eee90b59214175b30b86dd091b3157144f3a929c
Shubham Lakhiwal <shubham.lakhiwal@hashedin.com>
2012-12-29 00:32:23 +0530

    Class span3 replicated.

--- js/date.js
+++ js/date.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
-  $("p").click(function(){
-    $(this).hide(5000);
-  });
+	var i = 0;
+	$("p").click(function(){
+		$(this).hide(500,function(){alert("It is now hidden.")});
+	});
+	$("#addButton").click(function(){
+		$($(".replicate")[i++]).clone().insertBefore(".addBefore")
+		$($(".replicate")[i]).html("Hello");
+		$($(".replicate")[i]).html("Hello");
+		alert(i);
+	});
 });
 
-alert($("p").size());
 
-$("#1234").hide(500);
+
+//alert($("p").size());
+
+//$("#1234").hide(2000);
