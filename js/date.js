e0f1f39a1ae964c3957054d786a9eaa34b221495
Shubham Lakhiwal <shubham.lakhiwal@hashedin.com>
2012-12-29 08:48:48 +0530

    Not able to add task to a collaborator.

--- js/date.js
+++ js/date.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
 	var i = 0;
+	var j=0;
 	$("p").click(function(){
 		$(this).hide(500,function(){alert("It is now hidden.")});
 	});
 	$("#addButton").click(function(){
 		$($(".replicate")[i++]).clone().insertBefore(".addBefore")
-		$($(".replicate")[i]).html('<p>'+$("#textInput").val()+'</p><nav><ul>  <!-- Unordered List --><li >In progress.</li></ul></nav><input type="text" id="ListInput"><button id="listAddButton">Add</button>');
+		$($(".replicate")[i]).html('<p>'+$("#textInput").val()+'</p><nav><ul class = "listAppend">  <!-- Unordered List --><li >In progress.</li></ul></nav><input type="text" id="listInput"><button class="listAddButton">Add</button>');
 		alert("Collaborator "+i+" : "+$("#textInput").val()+" added");
 		$("#textInput").val("");
 	});
-	
+	$(".listAddButton").click(function(){
+		$("li").clone().appendTo("li");
+		$("li").html("<li>"+$("#listInput").val()+"</li>");
+	});
 	
 });
 
